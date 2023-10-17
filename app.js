0fb3172748eef30a9c08189c008f191f600f424d
WebDevMullins <webdevmullins@gmail.com>
2023-10-17 10:35:50 -0500

    added GET /notes and * endpoints

--- app.js
+++ app.js
@@ -6,4 +6,7 @@ const PORT = 3001
 
 app.use(express.static('public'))
 
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')))
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')))
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`))
