fb337fa3274e4984fe0cc4868e0c02882b23f84c
WebDevMullins <webdevmullins@gmail.com>
2023-10-17 11:00:42 -0500

    add GET /api/notes to read db.json

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const path = require('path')
+const dbData = require('./db/db.json')
 
 const app = express()
 const PORT = 3001
 
 app.use(express.static('public'))
 
+app.get('/api/notes', (req, res) => {
+	res.json(dbData)
+})
+
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')))
 
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')))
+
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`))
