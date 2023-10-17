a635c0d58c05d1628e6df43ea5922cc1df873242
WebDevMullins <webdevmullins@gmail.com>
2023-10-17 16:59:23 -0500

    add DELETE route to remove note

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const { nanoid } = require('nanoid')
 const fs = require('fs')
 const path = require('path')
-const dbData = require('./db/db.json')
+
+const db = require('./db/db.json')
 
 const app = express()
 const PORT = 3001
@@ -11,18 +12,18 @@ app.use(express.json())
 
 app.use(express.static('public'))
 
-app.get('/api/notes', (req, res) => res.json(dbData))
+app.get('/api/notes', (req, res) => res.json(db))
 
 app.post('/api/notes', (req, res) => {
-	console.info(`${req.method} request received to add a review`)
+	console.info(`${req.method} request received to add a note`)
 
 	const { title, text } = req.body
 
 	const newNote = { title, text, id: nanoid(5) }
 
-	dbData.push(newNote)
+	db.push(newNote)
 
-	fs.writeFile(path.join(__dirname, 'db', 'db.json'), JSON.stringify(dbData), (err) => {
+	fs.writeFile(path.join(__dirname, 'db', 'db.json'), JSON.stringify(db), (err) => {
 		if (err) {
 			console.error('Error writing to db.json', err)
 		} else {
@@ -32,6 +33,22 @@ app.post('/api/notes', (req, res) => {
 			}
 			console.log(response)
 			res.status(201).json(response)
+		}
+	})
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+	console.info(`${req.method} request received to remove note with id ${req.params.id}`)
+
+	const delNoteById = db.filter((note) => note.id !== req.params.id)
+
+	fs.writeFile('./db/db.json', JSON.stringify(delNoteById), (err) => {
+		if (err) {
+			console.error('Error deleting note', err)
+			res.status(500).json('Failed to delete the note')
+		} else {
+			console.log('Deleted note')
+			res.status(204).json('Deleted note')
 		}
 	})
 })
