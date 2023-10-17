a169ff88ec768c1cb2850b960ba6081d837b62db
WebDevMullins <webdevmullins@gmail.com>
2023-10-17 10:16:09 -0500

    update to use static middleware

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const path = require('path')
 const app = express()
 const PORT = 3001
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/')))
+app.use(express.static('public'))
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`))
