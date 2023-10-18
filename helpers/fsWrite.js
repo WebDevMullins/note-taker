f930f996f9401c0211f7ecb585400ac69c195e26
WebDevMullins <webdevmullins@gmail.com>
2023-10-18 16:08:03 -0500

    update DELETE to check if id exists in array before trying to remove note

--- helpers/fsWrite.js
+++ helpers/fsWrite.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 
 const writeToFile = (file, content) =>
 	fs.writeFile(file, JSON.stringify(content, null, 4), (err) =>
-		err ? console.error(err) : console.info(`\nData written to ${file}`)
+		err ? console.error(err) : console.info(`\n${file} has been updated`)
 	)
 
 module.exports = writeToFile
--- routes/notes.js
+++ routes/notes.js
@@ -1,28 +1,27 @@
 const notes = require('express').Router()
-const fs = require('fs')
-const path = require('path')
 const { nanoid } = require('nanoid')
 const db = require('../db/db.json')
 const writeToFile = require('../helpers/fsWrite')
 
 // GET Route for retrieving all notes
-notes.get('/', (req, res) => res.json(db))
+notes.get('/', (req, res) => res.status(200).json(db))
 
-//  POST route for adding a note
+// POST route for adding a note
 notes.post('/', (req, res) => {
-	console.info(`${req.method} request received to add a note`)
-
+	// Log POST method was made
+	console.log(`${req.method} request received to add a note`)
+	// Deconstruct body
 	const { title, text } = req.body
-
+	// Validation to make sure body contains both a title and text
 	if (title && text) {
 		const newNote = {
 			title,
 			text,
 			id: nanoid(5)
 		}
-
+		// Add note to db array
 		db.push(newNote)
-
+		// Write to file with added note
 		writeToFile('./db/db.json', db)
 
 		const response = {
@@ -30,26 +29,34 @@ notes.post('/', (req, res) => {
 			body: newNote
 		}
 
-		res.json(response)
+		res.status(201).json(response)
 	} else {
-		res.json('Error adding note')
+		res.console.error('Error adding note')
 	}
 })
-
+// DELETE route for removing a note
 notes.delete('/:id', (req, res) => {
-	console.info(`${req.method} request received to remove note with id ${req.params.id}`)
+	// Set params.id
+	const noteId = req.params.id
+	// Log Delete method was make along with the note id
+	console.log(`${req.method} request received to remove note with id ${noteId}`)
+	// Check to see if note id exists in array
+	const findId = db.some((note) => note.id === noteId)
 
-	const delNoteById = db.filter((note) => note.id !== req.params.id)
+	if (findId) {
+		// If note id exists, filter out note with id
+		const delNoteById = db.filter((note) => note.id !== noteId)
+		// Write to file with the note removed
+		writeToFile('./db/db.json', delNoteById)
 
-	fs.writeFile('./db/db.json', JSON.stringify(delNoteById), (err) => {
-		if (err) {
-			console.error('Error deleting note', err)
-			res.status(500).json('Failed to delete the note')
-		} else {
-			console.log('Deleted note')
-			res.status(204).json('Deleted note')
-		}
-	})
+		console.log(`Deleted note with id ${noteId}`)
+
+		res.sendStatus(204)
+	} else {
+		console.log(`Error deleting note ${noteId}`)
+
+		res.status(500).json(`Error deleting note ${noteId}`)
+	}
 })
 
 module.exports = notes
